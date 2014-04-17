3807e3d9bd49553dfc45630ef95699fed71f0520
Andrej Bauer <Andrej.Bauer@andrej.com>
2014-04-18 00:57:54 +0200

    priprava za vaje

--- predikatni_racun.v
+++ predikatni_racun.v
@@ -10,26 +10,36 @@ Require Import Omega.
 Theorem vaja_1:
   forall n : nat, exists m : nat, n < m.
 Proof.
-  (* naredimo na predavanjih *)
-  admit.
+  intro n.
+  exists (S n). (* naslednik od n *)
+  omega.
 Qed.
 
 Theorem vaja_2 (n : nat): exists m : nat, n < m.
 Proof.
-  (* naredimo na predavanjih *)
-  admit.
+  exists (1 + n).
+  omega.
 Qed.
 
 Theorem vaja_4 (n m : nat): 2 * n + m + 1 = m + n + 1 + n.
 Proof.
-  (* naredimo na predavanjih *)
-  admit.
+  omega.
 Qed.
 
 Theorem vaja_5 (n : nat): (exists m : nat, n = 2 * m) \/ (exists m : nat, n = 1 + 2 * m).
 Proof.
-  (* naredimo na predavanjih *)
-  admit.
+  induction n.
+  - left.
+    exists 0 ; auto.
+  - destruct IHn.
+    + right.
+      destruct H as [k G].
+      exists k.
+      omega.
+    + left.
+      destruct H as [k G].
+      exists (k + 1).
+      omega.
 Qed.
 
 Theorem vaja_6: forall n, exists m, n = 3 * m \/ n = 3 * m + 1 \/ n = 3 * m + 2.
@@ -39,7 +49,7 @@ Proof.
 Qed.
 
 (* Predpostavimo, da imamo množici A in B. *)
-Parameter A B : Set. 
+Parameter A B : Set.
 
 (* Predpostavimo, da imamo predikat P na A in  Q na B. *)
 Parameter P : A -> Prop.
@@ -48,8 +58,8 @@ Parameter Q : B -> Prop.
 Theorem vaja_7:
   forall y : B, (forall x : A , P x /\ Q y) -> (forall z : A, P z).
 Proof.
-  (* naredimo na predavanjih *)
-  admit.
+  intros y H z.
+  apply H.
 Qed.
 
 (* Predpostavimo relacijo R med A in B. *)
@@ -84,8 +94,18 @@ Qed.
 
 Lemma vaja_11: dn -> lem.
 Proof.
-  (* naredimo na predavanjih *)
-  admit.
+  unfold dn, lem.
+  intros D Q.
+  apply D.
+  intro G.
+  absurd Q.
+  - intro.
+    apply G.
+    left ; assumption.
+  - apply D.
+    intro L.
+    apply G.
+    right ; assumption.
 Qed.
   
 Theorem vaja_12:
@@ -96,8 +116,16 @@ Proof.
 Qed.
 
 Theorem vaja_13:
-  dn -> (~ forall x : A, ~ P x) -> exists x : A, P x.
+  dn -> ~ (forall x : A, ~ P x) -> exists x : A, P x.
 Proof.
-  (* naredimo na vajah *)
-  admit.
+  intros D H.
+  apply D.
+  intro G.
+  absurd (forall x : A, ~ P x).
+  - assumption.
+  - intros y K.
+    absurd (exists z : A, P z).
+    + assumption.
+    + exists y.
+      assumption.
 Qed.
